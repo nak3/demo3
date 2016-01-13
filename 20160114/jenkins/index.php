db071c2bdfc2805ef55bfa939c9089edd3f78a92
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2016-01-13 15:26:02 +0900

    Add sample app for jenkins delivery pipeline

---
+++ 20160114/jenkins/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>OpenShift v3 demonstration</title>
+</head>
+<body>
+<h1>
+<?php
+  $kakushi = htmlspecialchars($_GET['kakushi']);
+  echo "{$kakushi}";
+?>
+</h1>
+<form action="index.php" method="get">
+  <input type="hidden" name="kakushi" value="Good Morning OpenShift v3!">
+  <input type="submit">
+</form>
+
+</body>
+</html>
